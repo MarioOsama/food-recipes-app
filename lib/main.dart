01989b4a58059e0e2e468db9dc56a0c79ecc5439
MarioOsama <93713938+Mario-Osama@users.noreply.github.com>
2024-07-08 11:33:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_recipes_app/core/routing/app_router.dart';
 import 'package:food_recipes_app/food_recipes_app.dart';
 import 'package:food_recipes_app/prefs/shared_preferences.dart';
-// This is the test branch
+// This is the test-2 branch
 void main() async {
   bool isFirstTime = await isNewUser;
   AppRouter appRouter = AppRouter(isFirstTime: isFirstTime);
