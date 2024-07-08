98a2a04aaa0d95a8769c9effe9880997ce1f65c1
MarioOsama <marioosamafayez@gmail.com>
2024-07-08 13:47:34 +0300

    Finish Project SetupⓂ️
    - Relocated the shared preference folder in core.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_recipes_app/core/di/dependency_injection.dart';
 import 'package:food_recipes_app/core/routing/app_router.dart';
 import 'package:food_recipes_app/food_recipes_app.dart';
-import 'package:food_recipes_app/prefs/shared_preferences.dart';
+import 'package:food_recipes_app/core/prefs/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
