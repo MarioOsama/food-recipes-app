fc7c3215ecc4eff7fe7caec3e91b0c1551fed7a8
MarioOsama <marioosamafayez@gmail.com>
2024-07-08 13:42:03 +0300

    Finish Project SetupⓂ️
    - Remove home attribute from MaterialApp.
    - Added widgets flutter binding and setupGetIt method into main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipes_app/core/di/dependency_injection.dart';
 import 'package:food_recipes_app/core/routing/app_router.dart';
 import 'package:food_recipes_app/food_recipes_app.dart';
 import 'package:food_recipes_app/prefs/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize dependency injection
+  setupGetIt();
+  // Check if the user is new
   bool isFirstTime = await isNewUser;
   AppRouter appRouter = AppRouter(isFirstTime: isFirstTime);
-
   runApp(
     FoodRecipesApp(
       appRouter: appRouter,
