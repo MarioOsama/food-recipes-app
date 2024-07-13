41a54d540ccf3506606fa5e49d0b3dc4f09bfed7
MarioOsama <marioosamafayez@gmail.com>
2024-07-13 13:10:50 +0300

    Refactor File pathsⓂ️

--- lib/features/Auth/ui/auth_screen.dart
+++ lib/features/Auth/ui/auth_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_recipes_app/core/theming/app_colors.dart';
 import 'package:food_recipes_app/core/theming/app_text_styles.dart';
-import 'package:food_recipes_app/core/utils/app_assets/app_assets.dart';
+import 'package:food_recipes_app/core/app_assets/app_assets.dart';
 import 'package:food_recipes_app/features/Auth/ui/custom_login_screen.dart';
 import 'package:food_recipes_app/features/Auth/ui/custom_register_screen.dart';
 
