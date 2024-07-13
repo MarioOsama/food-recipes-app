7fb36546694b460a00f688a041a5c4d8758deb4d
MarioOsama <marioosamafayez@gmail.com>
2024-07-13 14:22:40 +0300

    Setup Recipe Details Screen Env.Ⓜ️
    - Refactored the `RecipeItemModel` to fit the new attributes that needed in `RecipeScreen`.
    - Added `RecipeScreen` app router.
    - Mapped the home recipes to new `RecipeItemModel` attributes.
    - Created a `RecipeScreen` widget to show recipe details.

--- lib/core/models/recipe_item_model.dart
+++ lib/core/models/recipe_item_model.dart
@@ -1,7 +1,27 @@
 class RecipeItemModel {
+  final String id;
   final String title;
   final String imageUrl;
+  final String category;
+  final String steps;
+  final List<String> ingredients;
+  final List<String> measures;
   final String? subtitle;
+  final String? tags;
+  final String? glass;
+  final String? youTubeVideoUrl;
 
-  RecipeItemModel({required this.title, required this.imageUrl, this.subtitle});
+  RecipeItemModel({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.category,
+    required this.steps,
+    required this.ingredients,
+    required this.measures,
+    this.subtitle,
+    this.tags,
+    this.glass,
+    this.youTubeVideoUrl,
+  });
 }
---
+++ lib/features/recipe/ui/recipe_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RecipeScreen extends StatelessWidget {
+  const RecipeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Center(
+            child: Text('Recipe Screen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
