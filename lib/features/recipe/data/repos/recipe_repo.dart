2a08cb3180be9e0b0a815ae02bdc1d6222cf2599
MarioOsama <marioosamafayez@gmail.com>
2024-07-14 17:43:00 +0300

    Finish Favorite LogicⓂ️
    - Implemented the `save` `get` `remove` favorites in `shared_preferences` file, `RecipeRepo` and `RecipeCubit`.
    - Added `isFavorite` attribute to `RecipeSuccess` state to build the favorite button depends on.
    - Removed the favorite button from the shimmer widget and the recipe error screen for more reliability.

--- lib/features/recipe/data/repos/recipe_repo.dart
+++ lib/features/recipe/data/repos/recipe_repo.dart
@@ -2,6 +2,7 @@ import 'package:food_recipes_app/core/models/cocktail_response_model.dart';
 import 'package:food_recipes_app/core/models/food_response_model.dart';
 import 'package:food_recipes_app/core/networking/cocktail_api_services.dart';
 import 'package:food_recipes_app/core/networking/food_api_services.dart';
+import 'package:food_recipes_app/core/prefs/shared_preferences.dart';
 
 class RecipeRepo {
   final FoodApiServices _foodApiServices;
@@ -20,5 +21,18 @@ class RecipeRepo {
     final CocktailResponseModel cocktailRecipeData =
         await _cocktailApiServices.getByID(id);
     return cocktailRecipeData.cocktails.first;
+  }
+
+  // Favorite
+  Future<void> saveRecipeToSharedPrefs(String recipeId) async {
+    await setFavorite(recipeId);
+  }
+
+  Future<void> removeRecipeFromSharedPrefs(String recipeId) async {
+    await removeFavorite(recipeId);
+  }
+
+  Future<List<String>> getFavoritesFromSharedPrefs() async {
+    return await getFavorites();
   }
 }
--- lib/features/recipe/ui/widgets/recipe_screen_error.dart
+++ lib/features/recipe/ui/widgets/recipe_screen_error.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food_recipes_app/core/helpers/spacing.dart';
 import 'package:food_recipes_app/core/theming/app_colors.dart';
 import 'package:food_recipes_app/core/theming/app_text_styles.dart';
-import 'package:food_recipes_app/features/recipe/ui/widgets/recipe_screen_header.dart';
 
 class RecipeScreenError extends StatelessWidget {
   const RecipeScreenError({
@@ -18,7 +17,19 @@ class RecipeScreenError extends StatelessWidget {
         padding: EdgeInsets.only(top: 60.h),
         child: Column(
           children: [
-            const RecipeScreenHeader(),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.keyboard_arrow_left_sharp,
+                  size: 32.r,
+                  color: AppColors.black,
+                ),
+              ),
+            ),
             verticalSpace(25),
             const Center(
               child: Text(
