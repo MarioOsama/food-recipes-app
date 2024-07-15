a6604e79dc4d3508464065328179b2a5e6ee63aa
Khaled Dreat <dreatkhaled@gmail.com>
2024-07-16 01:02:48 +0300

    Add SimpleBlocObserver and Firebase initialization
    This commit adds a SimpleBlocObserver to log changes in BLoCs and initializes Firebase in the main function using the default Firebase options for the current platform.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_recipes_app/core/di/dependency_injection.dart';
 import 'package:food_recipes_app/core/routing/app_router.dart';
 import 'package:food_recipes_app/food_recipes_app.dart';
 import 'package:food_recipes_app/core/prefs/shared_preferences.dart';
 
+import 'core/simple_bloc_observer/simple_bloc_observer.dart';
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Initialize dependency injection
   setupGetIt();
   // Check if the user is new
+  Bloc.observer = SimpleBlocObserver();
+
   bool isFirstTime = await isNewUser;
   AppRouter appRouter = AppRouter(isFirstTime: isFirstTime);
   runApp(
