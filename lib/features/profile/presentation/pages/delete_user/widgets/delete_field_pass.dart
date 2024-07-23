f3c1aa37f9e0a7171db2041c13fc6cfbe3afa43b
Khaled Dreat <dreatkhaled@gmail.com>
2024-07-23 14:07:07 +0300

    Refactor delete_field_pass.dart
    - Import Easy Localization and AppString helper class
    - Translate label text using Easy Localization's tr() method

--- lib/features/profile/presentation/pages/delete_user/widgets/delete_field_pass.dart
+++ lib/features/profile/presentation/pages/delete_user/widgets/delete_field_pass.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_recipes_app/core/helpers/app_string.dart';
 import 'package:food_recipes_app/features/profile/presentation/cubit/profile_cubit.dart';
 import '../../../../../../core/validators/app_validators.dart';
 import '../../../widgets/custom_text_filed.dart';
@@ -10,7 +12,7 @@ class DeleteFieldPass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomTextFiled(
-      label: "Current Password",
+      label: AppString.currentPassword.tr(),
       isPass: true,
       validator: AppValidators.checkPass,
       onSaved: (value) =>
