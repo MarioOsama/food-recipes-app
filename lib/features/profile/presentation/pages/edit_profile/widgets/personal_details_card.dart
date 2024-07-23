d24bc7969a4ff7a47ae1f57edfbbc2bb181d2b12
Khaled Dreat <dreatkhaled@gmail.com>
2024-07-23 14:11:33 +0300

    Refactor personal_details_card widget imports and remove unused imports.

--- lib/features/profile/presentation/pages/edit_profile/widgets/personal_details_card.dart
+++ lib/features/profile/presentation/pages/edit_profile/widgets/personal_details_card.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../../../../../core/helpers/app_string.dart';
 import '../../../../../../core/theming/app_colors.dart';
 import '../../../../../../core/theming/app_text_styles.dart';
