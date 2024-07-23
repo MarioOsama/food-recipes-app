1f8abde4260d8c869b4bfdf4cc0594b1833593a5
Khaled Dreat <dreatkhaled@gmail.com>
2024-07-23 14:05:13 +0300

    Add new strings for Recipe and Profile screens
    Added new strings for the Recipe and Profile screens to improve localization and user experience. The new strings include ingredients, steps, resetPassword, deleteProfile, currentPassword, newPassword, reNewPassword, myProfile, personalDetails, editProfile, preferences, contactUs, userName, email, phoneNumber, saveChanges, and warningUser.

--- lib/core/helpers/app_string.dart
+++ lib/core/helpers/app_string.dart
@@ -4,4 +4,25 @@ class AppString {
   static const String foods = 'Foods';
   static const String cocktails = 'Cocktails';
   static const String seeMore = 'See More';
+
+  // Recipe Screen
+  static const String ingredients = 'Ingredients';
+  static const String steps = 'steps';
+
+  // Profile Screen
+  static const String resetPassword = 'resetPassword';
+  static const String deleteProfile = 'deleteProfile';
+  static const String currentPassword = 'currentPassword';
+  static const String newPassword = 'newPassword';
+  static const String reNewPassword = 'reNewPassword';
+  static const String myProfile = 'myprofile';
+  static const String personalDetails = 'personalDetails';
+  static const String editProfile = 'editProfile';
+  static const String preferences = 'preferences';
+  static const String contactUs = 'contactUs';
+  static const String userName = 'userName';
+  static const String email = 'email';
+  static const String phoneNumber = 'phoneNumber';
+  static const String saveChanges = 'saveChanges';
+  static const String warningUser = 'warningUser';
 }
